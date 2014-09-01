cca0deec78d55190dbeff43c6a5a55367d249b60
Chris Travers <chris@efficito.com>
2014-09-01 07:23:45 -0700

    Packaging fixes

--- lib/PGObject/Util/BulkLoad.pm
+++ lib/PGObject/Util/BulkLoad.pm
@@ -15,11 +15,11 @@ PGObject::Util::BulkUpload - Bulk Upload records into PostgreSQL
 
 =head1 VERSION
 
-Version 0.01
+Version 0.02
 
 =cut
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 
 =head1 SYNOPSIS
